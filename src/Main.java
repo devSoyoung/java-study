f3de18113a1e93a0703d768a906e7f69ed60d126
SoyoungLee <soyoung.dev@gmail.com>
2018-12-19 16:34:41 +0900

    Add Student Class

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,56 @@
 
 import java.util.Scanner;
 
+class Student {
+    String name;
+    String major;
+}
+
 public class Main {
-    public static boolean checkNumber(int inputNum) {
+    private static boolean checkNumber(int inputNum) {
         if (inputNum == 4) return true;
         return false;
     }
 
+    private static void printLessOrMore(int inputNumber) {
+        if (inputNumber > 4 && inputNumber <= 10) {
+            System.out.println("그것보단 적어요");
+        } else if (inputNumber >= 0 && inputNumber < 4) {
+            System.out.println("그것보단 많아요.");
+        } else {
+            System.out.println("주어진 범위를 벗어났어요.");
+        }
+    }
+
+    private static void printStudents(Student[] students) {
+        System.out.println("학생들의 이름은");
+        for (Student student : students) {
+            System.out.println(student.name);
+        }
+        System.out.println("입니다.");
+    }
+
     public static void main(String[] args) {
-        String[] students = {"김철수", "박영희", "홍길동", "이춘향"};
+        String[] studentNames = {"김철수", "박영희", "홍길동", "이춘향"};
+        Student[] students = new Student[4];
+        for(int i = 0; i < 4; i++) {
+            students[i] = new Student();
+            students[i].name = studentNames[i];
+        }
+
         Scanner sc = new Scanner(System.in);
         System.out.println("학생은 모두 몇 명일까요?\n0-10까지 생각나는 숫자를 하나 입력하세요.");
         while (sc.hasNextInt()) {
-            byte inputNumber = (byte)sc.nextInt();
+            byte inputNumber = (byte) sc.nextInt();
             if (checkNumber(inputNumber)) {
-                System.out.println("정답입니다. 학생들의 이름은");
-                for (String student : students) {
-                    System.out.println(student);
-                }
-                System.out.println("입니다.");
+                System.out.println("정답입니다.");
+                printStudents(students);
                 break;
             } else {
                 System.out.println("틀렸습니다.");
-                switch(inputNumber) {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                        System.out.println("그것보단 많아요.");
-                        break;
-                    default:
-                        System.out.println("그것보단 작아요.");
-                        break;
-                }
+                printLessOrMore(inputNumber);
+                System.out.println("다른 숫자를 다시 입력해주세요.");
             }
-            System.out.println("다른 숫자를 다시 입력해주세요.");
         }
         sc.close();
     }
